161053e357e4df03030d9f34851d29bcc3023784
Hagen Larsen <hagenjay7@outlook.com>
2022-08-23 13:50:31 -0700

    changed graph type on forecast

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework.serializers import ModelSerializer
 from accounts.models import *
 
-class AccountSerializer(ModelSerializer):
-    class Meta:
-        model = Account
-        fields = '__all__'
-
 
 class AccountCheckpointSerializer(ModelSerializer):
     class Meta:
         model = AccountCheckpoint
         fields = '__all__'
+
+
+class AccountSerializer(ModelSerializer):
+    class Meta:
+        model = Account
+        fields = '__all__'
+
+    
--- forecasts/static/forecasts/forecasts.js
+++ forecasts/static/forecasts/forecasts.js
@@ -1,93 +1,50 @@
+accounts = fetch('/accounts/api/get-checkpoints/', {
+  method: 'GET',
+  credentials: 'same-origin',
+  headers: {
+      'Content-Type': 'application/json',
+      "Accept": "application/json"
+      },
+      }).then(response => response.json()).then(data => {
+          console.log(data);
+          return data;
+      });
+
 const DATA_COUNT = 7;
 const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: 100};
-let data = {}
-
-accounts = fetch('/accounts/api/get-checkpoints/', {
-    method: 'GET',
-    credentials: 'same-origin',
-    headers: {
-        'Content-Type': 'application/json',
-        "Accept": "application/json"
-        },
-        }).then(response => response.json()).then(data => {
-            console.log(data);
-            return data;
-        });
-        
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-data = {
-  labels: labels,
-  datasets: [
-    {
-      label: 'My First dataset',
-      data: [50, 100, 150, 200, 250, 300, 350],
-      borderColor: '#00bcd4',
-      backgroundColor: '00bcd4',
-      fill: true
+const data = [
+  {x: 'Jan', net: 100, cogs: 50, gm: 50}, 
+  {x: 'Feb', net: 120, cogs: 55, gm: 75}
+];
+const cfg = {
+    type: 'bar',
+    data: {
+        labels: ['Jan', 'Feb'],
+        datasets: [{
+            label: 'Net sales',
+            data: data,
+            parsing: {
+                yAxisKey: 'net'
+            }
+        }, {
+            label: 'Cost of goods sold',
+            data: data,
+            parsing: {
+                yAxisKey: 'cogs'
+            }
+        }, {
+            label: 'Gross margin',
+            data: data,
+            parsing: {
+                yAxisKey: 'gm'
+            }
+        }]
     },
-    {
-      label: 'My Second dataset',
-      data: [50, 100, 150, 200, 250, 300, 350],
-      borderColor: '#ffc107',
-      backgroundColor: '#ffc107',
-      fill: true
-    },
-    {
-      label: 'My Third dataset',
-      data: [50, 100, 150, 200, 250, 300, 350],
-      borderColor: '#ff5722',
-      backgroundColor: '#ff5722',
-      fill: true
-    },
-    {
-      label: 'My Fourth dataset',
-      data: [50, 100, 150, 200, 250, 300, 350],
-      borderColor: '#03a9f4',
-      backgroundColor: '#03a9f4',
-      fill: true
-    }
-  ]
-};const config = {
-    type: 'line',
-    data: data,
-    options: {
-      responsive: true,
-      plugins: {
-        title: {
-          display: true,
-          text: (ctx) => 'Chart.js Line Chart - stacked=' + ctx.chart.options.scales.y.stacked
-        },
-        tooltip: {
-          mode: 'index'
-        },
-      },
-      interaction: {
-        mode: 'nearest',
-        axis: 'x',
-        intersect: false
-      },
-      scales: {
-        x: {
-          title: {
-            display: true,
-            text: 'Month'
-          }
-        },
-        y: {
-          stacked: true,
-          title: {
-            display: true,
-            text: 'Value'
-          }
-        }
-      }
-    }
-  };
- 
-
+};
+    
 const myChart = new Chart(
     document.getElementById('myChart'),
-    config
+    cfg
   );
 
 
